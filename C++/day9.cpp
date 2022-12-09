d5bfb87bc7950c1b8885053da96886c7854f5813
theverygaming <clemibunge@gmail.com>
2022-12-09 23:27:21 +0100

    day9 part2

--- C++/day9.cpp
+++ C++/day9.cpp
@@ -6,28 +6,26 @@
 #include <map>
 #include <vector>
 
-#define INITIAL_GRID_SIZE_Y 5
-#define INITIAL_GRID_SIZE_X 6
+#define INITIAL_GRID_SIZE_Y 21
+#define INITIAL_GRID_SIZE_X 26
 
 struct point {
     int x;
     int y;
 };
 
-enum class direction { UP, DOWN, LEFT, RIGHT };
+enum class direction { UP, DOWN, LEFT, RIGHT, UPPER_RIGHT, UPPER_LEFT, LOWER_RIGHT, LOWER_LEFT };
 std::map<char, direction> direction_map = {{'R', direction::RIGHT}, {'L', direction::LEFT}, {'U', direction::UP}, {'D', direction::DOWN}};
 
-enum class exact_direction { UP, DOWN, LEFT, RIGHT, UPPER_RIGHT, UPPER_LEFT, LOWER_RIGHT, LOWER_LEFT };
-
-static void print_grid(struct point *start, struct point *tail, struct point *head, size_t size_x, size_t size_y, std::vector<struct point> *tailpositions) {
+static void print_grid(std::vector<struct point> *rope, size_t size_x, size_t size_y, std::vector<struct point> *tailpositions) {
     char(*grid)[size_x] = (char(*)[size_x])malloc(size_x * size_y);
     memset(grid, '.', size_y * size_x);
 
-    if (start != nullptr) {
-        grid[start->y][start->x] = 's';
+    for (ssize_t i = rope->size() - 1; i > 0; i--) {
+        grid[(*rope)[i].y][(*rope)[i].x] = '0' + i;
     }
-    grid[tail->y][tail->x] = 'T';
-    grid[head->y][head->x] = 'H';
+
+    grid[(*rope)[0].y][(*rope)[0].x] = 'H';
 
     if (tailpositions != nullptr) {
         for (size_t i = 0; i < tailpositions->size(); i++) {
@@ -51,17 +49,17 @@ static size_t get_point_distance(struct point *a, struct point *b) {
     return sqrt(pow(b->x - a->x, 2) + pow(b->y - a->y, 2));
 }
 
-static exact_direction get_direction(struct point *tail, struct point *head) {
+static direction get_direction(struct point *tail, struct point *head) {
     if (tail->x == head->x) { // vertical
         if (tail->y < head->y) {
-            return exact_direction::DOWN;
+            return direction::DOWN;
         }
-        return exact_direction::UP;
+        return direction::UP;
     } else if (tail->y == head->y) { // horizontal
         if (tail->x < head->x) {
-            return exact_direction::RIGHT;
+            return direction::RIGHT;
         }
-        return exact_direction::LEFT;
+        return direction::LEFT;
     }
     float angle = atan2(head->y - tail->y, head->x - tail->x) * (180 / M_PI);
     /*
@@ -70,14 +68,14 @@ static exact_direction get_direction(struct point *tail, struct point *head) {
      */
     if (angle < 0) {
         if (-angle > 90) {
-            return exact_direction::UPPER_LEFT;
+            return direction::UPPER_LEFT;
         }
-        return exact_direction::UPPER_RIGHT;
+        return direction::UPPER_RIGHT;
     } else {
         if (angle > 90) {
-            return exact_direction::LOWER_LEFT;
+            return direction::LOWER_LEFT;
         }
-        return exact_direction::LOWER_RIGHT;
+        return direction::LOWER_RIGHT;
     }
     exit(1);
 }
@@ -85,31 +83,31 @@ static exact_direction get_direction(struct point *tail, struct point *head) {
 static void adjust_tail(struct point *tail, struct point *head) {
     if (get_point_distance(tail, head) > 1) {
         switch (get_direction(tail, head)) {
-        case exact_direction::UP:
+        case direction::UP:
             tail->y--;
             break;
-        case exact_direction::DOWN:
+        case direction::DOWN:
             tail->y++;
             break;
-        case exact_direction::LEFT:
+        case direction::LEFT:
             tail->x--;
             break;
-        case exact_direction::RIGHT:
+        case direction::RIGHT:
             tail->x++;
             break;
-        case exact_direction::UPPER_RIGHT:
+        case direction::UPPER_RIGHT:
             tail->y--;
             tail->x++;
             break;
-        case exact_direction::UPPER_LEFT:
+        case direction::UPPER_LEFT:
             tail->y--;
             tail->x--;
             break;
-        case exact_direction::LOWER_RIGHT:
+        case direction::LOWER_RIGHT:
             tail->y++;
             tail->x++;
             break;
-        case exact_direction::LOWER_LEFT:
+        case direction::LOWER_LEFT:
             tail->y++;
             tail->x--;
             break;
@@ -127,29 +125,31 @@ static void add_tail_position(struct point *tail, std::vector<struct point> *tai
     tailpositions->push_back(*tail);
 }
 
-static void move_head(direction dir, size_t count, struct point *tail, struct point *head, std::vector<struct point> *tailpositions) {
+static void move_head(direction dir, size_t count, std::vector<struct point> *rope, std::vector<struct point> *tailpositions) {
     for (size_t i = 0; i < count; i++) {
         switch (dir) {
         case direction::UP:
-            head->y--;
+            (*rope)[0].y--;
             break;
         case direction::DOWN:
-            head->y++;
+            (*rope)[0].y++;
             break;
         case direction::LEFT:
-            head->x--;
+            (*rope)[0].x--;
             break;
         case direction::RIGHT:
-            head->x++;
+            (*rope)[0].x++;
             break;
         }
-        adjust_tail(tail, head);
-        add_tail_position(tail, tailpositions);
-        //print_grid(nullptr, tail, head, INITIAL_GRID_SIZE_X, INITIAL_GRID_SIZE_Y, nullptr);
+        for (size_t i = 1; i < rope->size(); i++) {
+            adjust_tail(&(*rope)[i], &(*rope)[i - 1]);
+        }
+        add_tail_position(&(*rope)[rope->size() - 1], tailpositions);
     }
+    //print_grid(rope, INITIAL_GRID_SIZE_X, INITIAL_GRID_SIZE_Y, nullptr);
 }
 
-static void iamgoodatnamingfunctions(std::string filename) {
+static void part1(std::string filename) {
     std::ifstream infile(filename);
     if (!infile.is_open()) {
         fprintf(stderr, "could not open file %s\n", filename.c_str());
@@ -164,16 +164,18 @@ static void iamgoodatnamingfunctions(std::string filename) {
         }
     }
 
-    struct point start = {.x = 0, .y = INITIAL_GRID_SIZE_Y - 1};
-    struct point tail = start;
-    struct point head = start;
+    std::vector<struct point> rope;
+    rope.resize(2);
+    for (size_t i = 0; i < rope.size(); i++) {
+        rope[i] = {.x = 0, .y = INITIAL_GRID_SIZE_Y - 1};
+    }
 
     size_t size_x = INITIAL_GRID_SIZE_X;
     size_t size_y = INITIAL_GRID_SIZE_Y;
 
     std::vector<struct point> tailpositions;
 
-    add_tail_position(&tail, &tailpositions);
+    add_tail_position(&rope[1], &tailpositions);
 
     for (size_t l = 0; l < lines.size(); l++) {
         if (lines[l].size() == 0) {
@@ -184,14 +186,59 @@ static void iamgoodatnamingfunctions(std::string filename) {
         char dir_c;
         sscanf(lines[l].c_str(), "%c %llu", &dir_c, &count);
         direction dir = direction_map[dir_c];
-        move_head(dir, count, &tail, &head, &tailpositions);
+        move_head(dir, count, &rope, &tailpositions);
     }
 
-    //print_grid(&start, &tail, &head, size_x, size_y, &tailpositions);
+    // print_grid(&start, &tail, &head, size_x, size_y, &tailpositions);
     printf("solution part1: %llu\n", tailpositions.size());
 }
 
+static void part2(std::string filename) {
+    std::ifstream infile(filename);
+    if (!infile.is_open()) {
+        fprintf(stderr, "could not open file %s\n", filename.c_str());
+        throw std::runtime_error("could not open file");
+    }
+
+    std::vector<std::string> lines;
+    {
+        std::string currentLine;
+        while (std::getline(infile, currentLine)) {
+            lines.push_back(currentLine);
+        }
+    }
+
+    std::vector<struct point> rope;
+    rope.resize(10);
+    for (size_t i = 0; i < rope.size(); i++) {
+        rope[i] = {.x = 11, .y = 15};
+    }
+
+    size_t size_x = INITIAL_GRID_SIZE_X;
+    size_t size_y = INITIAL_GRID_SIZE_Y;
+
+    std::vector<struct point> tailpositions;
+
+    add_tail_position(&rope[1], &tailpositions);
+
+    for (size_t l = 0; l < lines.size(); l++) {
+        if (lines[l].size() == 0) {
+            continue;
+        }
+
+        size_t count;
+        char dir_c;
+        sscanf(lines[l].c_str(), "%c %llu", &dir_c, &count);
+        direction dir = direction_map[dir_c];
+        move_head(dir, count, &rope, &tailpositions);
+    }
+
+    //print_grid(&rope, size_x, size_y, &tailpositions);
+    printf("solution part2: %llu\n", tailpositions.size());
+}
+
 int main(int argc, char *argv[]) {
-    iamgoodatnamingfunctions("day9_input.txt");
+    part1("day9_input.txt");
+    part2("day9_input.txt");
     return 0;
 }
