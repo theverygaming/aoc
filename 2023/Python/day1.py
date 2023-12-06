04749c981109fe25eb7d9c31a731091a06fc628d
theverygaming <theverygaming@users.noreply.github.com>
2023-12-06 20:10:09 +0100

    day1 part2

--- 2023/Python/day1.py
+++ 2023/Python/day1.py
@@ -1,21 +1,27 @@
-def dictreplace(d, _t):
-    t = _t
-    for key, value in d.items():
-        t = t.replace(key, value)
-    return t
-
 def digits(_t, p2):
     t = _t
-    if p2:
-        # this won't work
-        t = dictreplace({"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}, t)
     arr = []
-    for n in range(0, len(t)):
-        if t[n].isdigit():
-            arr.append(int(t[n]))
+    if p2:
+        digits = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+        pos = 0
+        while pos < len(t):
+            ppos = pos
+            for k, v in digits.items():
+                if t[pos].isdecimal():
+                    arr.append(int(t[pos]))
+                    pos += 1
+                if t[pos:pos+len(k)] == k:
+                    pos += len(k) - 1
+                    arr.append(v)
+            if ppos == pos:
+                pos += 1
+    else:
+        for n in range(0, len(t)):
+            if t[n].isdigit():
+                arr.append(int(t[n]))
     return arr
 
-with open("day1_sample.txt") as f:
+with open("day1.txt") as f:
     sump1 = 0
     sump2 = 0
     for line in f.readlines():
@@ -23,8 +29,8 @@ with open("day1_sample.txt") as f:
             continue
         dp1 = digits(line, False)
         dp2 = digits(line, True)
-        np1 = (dp1[0] * 10) + dp1[-1]
-        np2 = (dp2[0] * 10) + dp2[-1]
+        np1 = (dp1[0] * 10) + dp1[-1] if len(dp1) > 0 else 0
+        np2 = (dp2[0] * 10) + dp2[-1] if len(dp2) > 0 else 0
         sump1 += np1
         sump2 += np2
         print(np2)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code
+
+[2022](2022/README.md)
+
+[2023](2023/README.md)
