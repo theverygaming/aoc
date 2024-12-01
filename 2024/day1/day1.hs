34cf0635054243715d098acd8fa10bf519b0d9ef
theverygaming <theverygaming@users.noreply.github.com>
2024-12-01 11:40:29 +0100

    day1 part 1

---
+++ 2024/day1/day1.hs
@@ -0,0 +1,20 @@
+import System.IO
+import Control.Monad
+import Data.List (sort)
+
+main = do
+    let listfull = []
+    -- file reading: https://stackoverflow.com/a/7867786
+    handle <- openFile "input.txt" ReadMode
+    contents <- hGetContents handle
+    let singlewords = words contents
+        listfull = map read singlewords :: [Int]
+    --print listfull
+    -- splitting the list https://www.reddit.com/r/haskell/comments/1cj8lc/comment/c9h2vxt
+    -- filtering list of tuples https://stackoverflow.com/a/1618477
+    let listleft = map (fst) (filter (even.snd) (zip listfull [0..]))
+    let listright = map (fst) (filter (odd.snd) (zip listfull [0..]))
+    --print listleft
+    --print listright
+    print (sum (map (\(a, b) -> abs (a - b)) (zip (sort listleft) (sort listright))))
+    hClose handle
